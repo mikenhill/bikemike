eabca381295438272ff0e1ca542de7eb99cdf596
Mike Hill <mikenhill@gmail.com>
2015-05-13 16:23:57 +0100

    Initial commit

---
+++ bikemike/src/main/java/com/mikenhill/bikemike/BikemikeApplication.java
@@ -0,0 +1,12 @@
+package com.mikenhill.bikemike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikemikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikemikeApplication.class, args);
+    }
+}
